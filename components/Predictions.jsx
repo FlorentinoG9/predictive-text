72715268f8265047b91f0bf3225d25f75a16e34d
FlorentinoG9 <f.valle.garcia@gmail.com>
2021-12-10 22:29:42 -0600

    minor changes handle overflow

--- components/Predictions.jsx
+++ components/Predictions.jsx
@@ -4,7 +4,7 @@ import { TextContext } from '../context/TextContext';
 export default function Predictions() {
 	const { text } = useContext(TextContext);
 	return (
-		<div className=''>
+		<div className=' overflow-hidden overflow-y-auto'>
 			<ul className='max-h-96 grid grid-cols-12 '>
 				{text.map((item, idx) => {
 					return <li key={idx}>{item}</li>;
